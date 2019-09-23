f3aa8e8908b37b3728d43f6264d6dd64e9229289
skpareek84 <skpareek2010@gmail.com>
2019-09-23 13:18:07 +0530

    This is final file

---
+++ test1.py
@@ -0,0 +1,10 @@
+print("Hello World")
+x=5
+y=7
+z="ok google"
+l=[2,5,x,y,z]
+
+
+print(l)
+print(z)
+print(y)
---
+++ test2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import time
+
+print ("Hello World")
+time.sleep(4)
+print ("Slept for 4 sec")
+
+print ("current date and time is ", time.ctime())
